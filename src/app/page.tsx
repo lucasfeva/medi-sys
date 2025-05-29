030aa3118c0a77fb2c3f677b3fe2a4393cf16107
lucasfeva <luscas.png@gmail.com>
2025-05-28 21:55:33 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Botão</Button>
+    </div>
+  );
 }
