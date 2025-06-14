f4d1582d12e472fb07febe1d5a3f94459d65b24e
lucasfeva <luscas.png@gmail.com>
2025-06-14 00:05:13 -0300

    fix: unsure doctor form is cleared when closed

--- src/components/doctors/upsert-doctor-form.tsx
+++ src/components/doctors/upsert-doctor-form.tsx
@@ -79,6 +79,7 @@ export default function UpsertDoctorForm({
   onSuccess,
 }: UpsertDoctorFormProps) {
   const form = useForm<z.infer<typeof formSchema>>({
+    shouldUnregister: true,
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: doctor?.name || "",
